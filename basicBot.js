1763e8fb16e919bd11d762c5eb18f7d4633d3226
Kslinghook <leagueofaway@hotmail.com>
2016-07-15 19:15:47 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2902,19 +2902,6 @@
                     }
                 }
             },
-                botsoffCommand: {
-                command: 'botsoff',
-                rank: 'bouncer',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        bots.util.sendChat('@' + msgData.un + ' ok, aguarde...');
-			bots.events.off();
-                    }
-                }
-            },
 
             removeCommand: {
                 command: 'remove',
