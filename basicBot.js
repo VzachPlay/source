38f28e0746d960560002ebf724bbb9cafee33a47
Kslinghook <leagueofaway@hotmail.com>
2016-07-15 18:38:32 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -240,9 +240,9 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+        scriptLink: "https://rawgit.com/Kslinghook/source/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
+        chatLink: "https://rawgit.com/Kslinghook/source/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -250,8 +250,8 @@
         settings: {
             botName: "basicBot",
             language: "english",
-            chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-            scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+            chatLink: "https://rawgit.com/Kslinghook/source/master/lang/en.json",
+            scriptLink: "https://rawgit.com/Kslinghook/source/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
@@ -309,9 +309,9 @@
             songstats: true,
             commandLiteral: "!",
             blacklists: {
-                NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-                OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-                BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+                NSFW: "https://rawgit.com/Kslinghook/custom/master/blacklists/NSFWlist.json",
+                OP: "https://rawgit.com/Kslinghook/custom/master/blacklists/OPlist.json",
+                BANNED: "https://rawgit.com/Kslinghook/custom/master/blacklists/BANNEDlist.json"
             }
         },
         room: {
