3dbb444c1c190ff86f31a109a1cad28a76f287a1
Kslinghook <leagueofaway@hotmail.com>
2016-07-15 20:11:54 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2898,7 +2898,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                            $.getScript('https://dl.dropboxusercontent.com/s/m3msel4fhxf1oas/bots.js');
+                            $.getScript('https://dl.dropboxusercontent.com/s/c0fi273ixkzyk8k/bots.js');
                     }
                 }
             },
