639e6259b9b6439571c2b621d324c4b6ffe75fe1
Kslinghook <leagueofaway@hotmail.com>
2016-07-20 18:36:41 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -1,6 +1,6 @@
 API.on(API.USER_JOIN, function(user) {
       if (user.username == "Awayshift") {
-         API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " chegou!.");
+         API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " entrou!.");
          }   
        if (user.username == "VzachPlay") {
          API.sendChat("/me O melhor segurança  @" + user.username + " entrou na sala!.");
