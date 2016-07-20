6ae24597ab5dbd881f15babc0ec7e4e3b4280b03
Kslinghook <leagueofaway@hotmail.com>
2016-07-20 17:51:38 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -1,3 +1,24 @@
+        main: {
+            init: function() {
+                bots.session.site = bots.util.detectSite();
+                 
+                if (bots.session.site == -1)
+                    return alert('Site nÃ£o reconhecido!');
+                 
+                if (bots.session.site == bots.consts.DUB)
+                    bots.util.loadDubCustomAPI();
+                 
+                bots.events.on();
+                setTimeout(function () {
+                 API.sendChat("!cmddel");;
+                 }, 2000);
+                bots.util.sendChat('SimSimi e Ed ativados');
+            }
+        }
+    };
+     
+    bots.main.init();
+})();
 API.sendChat("Olá");;
 API.on(API.USER_JOIN, function(user) {
       if (user.id == "6500812") {
