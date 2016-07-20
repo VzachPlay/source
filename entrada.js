780c70698842ace1f63a20b1b171d090e3e0cf55
Kslinghook <leagueofaway@hotmail.com>
2016-07-20 18:16:11 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -1,9 +1,5 @@
 API.on(API.USER_JOIN, function(user) {
-      if (user.id == "1380997") {
+      if (user.username == "Awayshift") {
          API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " chegou!.");
-         }   
-API.on(API.USER_BACK, function(user) {
-      if (user.id == "14638563") {
-         API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " Voltou!.");
          }   
         });
