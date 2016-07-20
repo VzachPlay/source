6bec8b2f0982c135e280bef63c3e92479b94777b
Kslinghook <leagueofaway@hotmail.com>
2016-07-20 16:54:38 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -1,8 +1,20 @@
 API.on(API.USER_JOIN, function(user) {
-     if (user.id == "4080795") {
-         API.sendChat("/me O gatão da sala @" + user.username + " chegou :cat:  ");
-         }
       if (user.id == "14638563") {
          API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " chegou!.");
          }    
+         var welcomeback = null;
+            if (known) {
+                basicBot.room.users[index].inRoom = true;
+                var u = basicBot.userUtilities.lookupUser(user.id);
+                var jt = u.jointime;
+                var t = Date.now() - jt;
+                if (t < 10 * 1000) greet = false;
+               if (user.id == "14638563") {
+               API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " chegou!.");
+               }    
+                else welcomeback = true;
+            }
+            else {
+                basicBot.room.users.push(new basicBot.User(user.id, user.username));
+                welcomeback = false;
         });
